fe33c9e53dfb3d1c6913d92534e26daaf6316d2c
Everton Kunz <ehokeverton@gmail.com>
2020-03-02 11:36:28 -0400

    Update README

--- lib/store/app_actions.dart
+++ lib/store/app_actions.dart
@@ -1,11 +1,3 @@
-class IncrementCounter {
-  IncrementCounter() {
-    print('Executou o action de increment');
-  }
-}
+class IncrementCounter {}
 
-class DecrementCounter {
-  DecrementCounter() {
-    print('Executou o action de decrement');
-  }
-}
+class DecrementCounter {}
